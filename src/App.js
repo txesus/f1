72cfd58bbd10845b0bd13867eb85caa04a57ee5a
Jesús Esteban <jesusesteban@MacBook-Pro-de-Jesus.local>
2019-07-27 00:57:39 +0200

    api request

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 
 import Map from './components/map/map';
+import ContentData from './components/content-data/content-data';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Map />        
+        <Map />   
+        {/* <ContentData /> */}
       </header>      
     </div>
   );
