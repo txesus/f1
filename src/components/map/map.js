bfa2e0ae5422c29dbcf603d3e2fddadf4ff4d717
Jesús Esteban <jesusesteban@MacBook-Pro-de-Jesus.local>
2019-07-27 19:33:32 +0200

    add marker

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Map, TileLayer } from 'react-leaflet';
-import Control from 'react-leaflet-control';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+// import Control from 'react-leaflet-control';
 
 const stamenTonerTiles = 'https://api.mapbox.com/styles/v1/jesusesteban/cjna67hy23vcf2rppfpvoj24q/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamVzdXNlc3RlYmFuIiwiYSI6ImNqc3VlY3EydTAxdDMzeXB2a2NycXJxZTIifQ.6Jxvu3C-J7-XWRjCVdMwdw';
 // const stamenTonerTiles = 'https://api.mapbox.com/styles/v1/jesusesteban/cjna67hy23vcf2rppfpvoj24q/wmts?access_token=https://api.mapbox.com/styles/v1/jesusesteban/cjna67hy23vcf2rppfpvoj24q/wmts?access_token=pk.eyJ1IjoiamVzdXNlc3RlYmFuIiwiYSI6ImNqc3VlY3EydTAxdDMzeXB2a2NycXJxZTIifQ.6Jxvu3C-J7-XWRjCVdMwdw';
@@ -11,14 +11,15 @@ const stamenTonerAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design<
 const mapCenter = [40.1301919, -8.2008354];
 const zoomLevel = 3;
 
-
+const circuitsLocation = [40.1301919, -8.2008354];
 
 export default class App extends Component {
     
     constructor(props) {
         super(props);
         this.state = { 
-            currentZoomLevel: zoomLevel
+            currentZoomLevel: zoomLevel,
+            resultados: []
         };
         this.handleUpPanClick = this.handleUpPanClick.bind(this);
         this.handleRightPanClick = this.handleRightPanClick.bind(this);
@@ -32,51 +33,58 @@ export default class App extends Component {
         leafletMap.on('zoomend', () => {
             const updatedZoomLevel = leafletMap.getZoom();
             this.handleZoomLevelChange(updatedZoomLevel);
-        });    
-    }
-
-    handleZoomLevelChange(newZoomLevel) {
-        this.setState({ currentZoomLevel: newZoomLevel });
-    }
-
-    handleUpPanClick() {
-        const leafletMap = this.leafletMap.leafletElement;
-        leafletMap.panBy([0, -100]);
-        window.console.log('Panning up');
-    }
-
-    handleRightPanClick() {
-        const leafletMap = this.leafletMap.leafletElement;
-        leafletMap.panBy([100, 0]);
-        window.console.log('Panning right');
-    }
-
-    handleLeftPanClick() {
-        const leafletMap = this.leafletMap.leafletElement;
-        leafletMap.panBy([-100, 0]);
-        window.console.log('Panning left');
-    }
-
-    handleDownPanClick() {
-        const leafletMap = this.leafletMap.leafletElement;
-        leafletMap.panBy([0, 100]);
-        window.console.log('Panning down');
-    }
-
-    render() {
-        // window.console.log('this.state.currentZoomLevel ->', this.state.currentZoomLevel);
-
-        return (
-            <div>
+        });  
+        fetch('https://ergast.com/api/f1/circuits.json?limit=100')
+            .then(response => response.json())
+            .then(resultadosJson => this.setState({resultados: resultadosJson.MRData.CircuitTable.Circuits}))
+            
+        }
+        
+        handleZoomLevelChange(newZoomLevel) {
+            this.setState({ currentZoomLevel: newZoomLevel });
+        }
+        
+        handleUpPanClick() {
+            const leafletMap = this.leafletMap.leafletElement;
+            leafletMap.panBy([0, -100]);
+            window.console.log('Panning up');
+        }
+        
+        handleRightPanClick() {
+            const leafletMap = this.leafletMap.leafletElement;
+            leafletMap.panBy([100, 0]);
+            window.console.log('Panning right');
+        }
+        
+        handleLeftPanClick() {
+            const leafletMap = this.leafletMap.leafletElement;
+            leafletMap.panBy([-100, 0]);
+            window.console.log('Panning left');
+        }
+        
+        handleDownPanClick() {
+            const leafletMap = this.leafletMap.leafletElement;
+            leafletMap.panBy([0, 100]);
+            window.console.log('Panning down');
+        }
+        
+        render() {
+            // window.console.log('this.state.currentZoomLevel ->', this.state.currentZoomLevel);
+            const { resultados } = this.state
+            
+            return (
+                <div>
                 <Map
                     ref={m => { this.leafletMap = m; }}
                     center={mapCenter}
                     zoom={zoomLevel}
                 >
-                    <TileLayer
-                        attribution={stamenTonerAttr}
-                        url={stamenTonerTiles}
-                    />
+                <TileLayer
+                    attribution={stamenTonerAttr}
+                    url={stamenTonerTiles}
+                />
+                <Marker position={circuitsLocation}></Marker>
+
                     {/* <Control position="topright">
                         <div>
                             <div style={{ marginLeft: '37px' }}>
