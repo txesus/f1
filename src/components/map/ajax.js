e427b44e5256eba2929be3370c046c32da0088c9
Jesus Esteban <jesusestebansanchez@gmail.com>
2019-07-28 16:14:29 +0200

    add circuits markers

--- src/components/map/ajax.js
+++ src/components/map/ajax.js
@@ -95,3 +95,7 @@ function recoveryId(select){
 }
 
 
+
+
+
+
--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Map, TileLayer, Marker, CircleMarker } from 'react-leaflet';
-// import Control from 'react-leaflet-control';
-
-//Data Circuits
-import data from './circuits'
 
 
 const stamenTonerTiles = 'https://api.mapbox.com/styles/v1/jesusesteban/cjna67hy23vcf2rppfpvoj24q/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamVzdXNlc3RlYmFuIiwiYSI6ImNqc3VlY3EydTAxdDMzeXB2a2NycXJxZTIifQ.6Jxvu3C-J7-XWRjCVdMwdw';
-// const stamenTonerTiles = 'https://api.mapbox.com/styles/v1/jesusesteban/cjna67hy23vcf2rppfpvoj24q/wmts?access_token=https://api.mapbox.com/styles/v1/jesusesteban/cjna67hy23vcf2rppfpvoj24q/wmts?access_token=pk.eyJ1IjoiamVzdXNlc3RlYmFuIiwiYSI6ImNqc3VlY3EydTAxdDMzeXB2a2NycXJxZTIifQ.6Jxvu3C-J7-XWRjCVdMwdw';
 
 // Ejemplos de mapas https://leaflet-extras.github.io/leaflet-providers/preview/
 const mapCenter = [39.9837669, -4.2810849];
 const zoomLevel = 3;
-
-const circuits = [];
 
 export default class App extends Component {
     
@@ -33,30 +26,29 @@ export default class App extends Component {
             const updatedZoomLevel = leafletMap.getZoom();
             this.handleZoomLevelChange(updatedZoomLevel);
         });     
-        fetch('https://ergast.com/api/f1/circuits.json?limit=100')
-            .then(response => response.json())
-            .then(resultadosJson => this.setState({resultados: resultadosJson.MRData.CircuitTable.Circuits}))
-
     }
-        
+
+    componentWillMount() {
+        fetch('https://ergast.com/api/f1/circuits.json?limit=100')
+          .then((response) => {
+            return response.json()
+          })
+          .then((resultados) => {
+            this.setState({ resultados: resultados.MRData.CircuitTable.Circuits})
+          })
+      }   
+
+
+
+
     handleZoomLevelChange(newZoomLevel) {
         this.setState({ currentZoomLevel: newZoomLevel });
     }
     
         
     render() {
-        
+
         const { resultados } = this.state;
-        console.log(resultados[0]);
-
-        // for (var i = 0; i < resultados.length; i++) {
-        //     const Latitud = resultados[i].Location.lat;
-        //     const Longitud = resultados[i].Location.long;            
-        //     console.log(position = {Latitud, Longitud});     
-        //     // break;           
-        // }
-
-        
 
         return (
             <div>
@@ -70,11 +62,15 @@ export default class App extends Component {
                     url={stamenTonerTiles}
                     />
 
-                {data.city.map((city) => {
+                {resultados.map((resultados) => {
                     return (
-                    <Marker
-                        position={[city["coordinates"][1], city["coordinates"][0]]}
-                    />)
+                        // <CircleMarker 
+                        //     center={[resultados.Location.lat, resultados.Location.long]}
+                        //     fillOpacity={0.5}
+                        //     stroke={false}                            
+                        // />
+                        <Marker position={[resultados.Location.lat, resultados.Location.long]} />
+                    )
                     })
                 }
 
