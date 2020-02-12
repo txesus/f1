d0b93e9bb1f2b747ed916119e02e97d9f8b33358
Esteban, Jesús <jesus.esteban@accenture.com>
2020-02-12 15:02:52 +0100

    gpx leaflet

--- src/components/select-year/select-year.js
+++ src/components/select-year/select-year.js
@@ -43,18 +43,15 @@ export default class SelectYear extends Component {
             const options = this.getOptions();
             return (
             <div>
-                {!!options[0] &&
-                    <div className={"select-year"}>
-                        <Select 
-                            options={options}     
-                            defaultValue={options[0]}
-                            onChange={e => {
-                                this.props.handleYearChange({target:{value: e.value}});
-                                this.props.handleResetZoom();
-                            }
-                            }      
-                        />
-                    </div>
+                <div className={"select-year"}>
+                    <Select 
+                        options={options}     
+                        onChange={e => {
+                            this.props.handleYearChange({target:{value: e.value}});
+                            this.props.handleResetZoom();
+                        }
+                        }      
+                    />
 
                 }
             </div>
